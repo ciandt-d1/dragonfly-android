b63b04b779596b02fd3cfc27d364aff5aedaa081
Ivam Luz <iluz@ciandt.com>
2017-06-02 16:07:38 -0300

    [SMTC-1367] Implement fade in animation for camera ornament

--- app/src/main/java/com/ciandt/dragonfly/example/shared/FullScreenActivity.kt
+++ app/src/main/java/com/ciandt/dragonfly/example/shared/FullScreenActivity.kt
@@ -21,18 +21,18 @@ abstract class FullScreenActivity(protected var keepScreenOn: Boolean = true) : 
         if (keepScreenOn) {
             window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
+
+        actionBar?.hide()
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        }
-    }
+    override fun onResume() {
+        super.onResume()
 
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    }
 }
