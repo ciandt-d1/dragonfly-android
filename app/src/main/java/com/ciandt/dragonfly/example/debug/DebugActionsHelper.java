d496629fa8818f1033b5a1c6c81234660f5f5345
Jean Pimentel <jpimentel@ciandt.com>
2017-08-07 16:16:02 -0300

    Cleanup unused codes

--- app/src/main/java/com/ciandt/dragonfly/example/debug/DebugActionsHelper.java
+++ app/src/main/java/com/ciandt/dragonfly/example/debug/DebugActionsHelper.java
@@ -5,19 +5,14 @@ import com.ciandt.dragonfly.example.data.ProjectRepository;
 import com.ciandt.dragonfly.example.data.remote.RemoteProjectService;
 import com.ciandt.dragonfly.example.features.login.LoginActivity;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.os.AsyncTaskCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -25,17 +20,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -100,32 +85,6 @@ public class DebugActionsHelper {
 
     public static List<ButtonAction> getButtonActions(final DebuggableActivity target) {
         final List<ButtonAction> actions = new ArrayList<>();
-
-        actions.add(new ButtonAction("Copy model from assets", new ButtonAction.Listener() {
-
-            @Override
-            public void onClick() {
-                Dexter.withActivity(target.getActivityInstance())
-                        .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        .withListener(new PermissionListener() {
-
-                            @Override
-                            public void onPermissionGranted(PermissionGrantedResponse response) {
-                                copyTensorFlowModelFromAssetsToExternalStorage(target);
-                            }
-
-                            @Override
-                            public void onPermissionDenied(PermissionDeniedResponse response) {
-                                Log.d(LOG_TAG, String.format("onPermissionRationaleShouldBeShown() - PermissionDeniedResponse: %s", response));
-                            }
-
-                            @Override
-                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-                                Log.d(LOG_TAG, String.format("onPermissionRationaleShouldBeShown() - PermissionRequest: %s, PermissionToken: %s", permission, token));
-                            }
-                        }).check();
-            }
-        }));
 
         actions.add(new ButtonAction("Logout", new ButtonAction.Listener() {
 
@@ -230,80 +189,6 @@ public class DebugActionsHelper {
         if (debugDrawer != null && debugDrawer.isDrawerOpen()) {
             debugDrawer.closeDrawer();
         }
-    }
-
-    private static void copyTensorFlowModelFromAssetsToExternalStorage(final DebuggableActivity target) {
-        AsyncTaskCompat.executeParallel(new AsyncTask<Void, Void, Boolean>() {
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-
-                Toast.makeText(target.getActivityInstance(), "Copying model from assets...", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            protected Boolean doInBackground(Void... params) {
-                Boolean result;
-                try {
-                    result = copyModelFromAssets("models/1/model.pb") && copyModelFromAssets("models/1/labels.txt");
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, e.getMessage(), e);
-                    result = false;
-                }
-
-                return result;
-
-            }
-
-            @NonNull
-            private Boolean copyModelFromAssets(String srcFilePath) throws Exception {
-                byte[] buffer = new byte[1024];
-
-                File outputFile = new File(target.getActivityInstance().getFilesDir(), srcFilePath);
-
-                InputStream is = null;
-                OutputStream os = null;
-
-                boolean result;
-                try {
-                    is = target.getActivityInstance().getAssets().open(srcFilePath);
-                    os = new FileOutputStream(outputFile);
-
-                    int n;
-                    while (-1 != (n = is.read(buffer))) {
-                        os.write(buffer, 0, n);
-                    }
-
-                    result = true;
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, e.getMessage(), e);
-
-                    result = false;
-                } finally {
-                    if (is != null) {
-                        is.close();
-                    }
-
-                    if (os != null) {
-                        os.close();
-                    }
-                }
-
-                return result;
-            }
-
-            @Override
-            protected void onPostExecute(Boolean result) {
-                super.onPostExecute(result);
-
-                if (result) {
-                    Toast.makeText(target.getActivityInstance(), "Model successfully copied!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(target.getActivityInstance(), "Failed to copy model!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 
     public interface DebuggableActivity {
