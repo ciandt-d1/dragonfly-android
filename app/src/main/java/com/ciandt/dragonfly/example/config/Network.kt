1ea83d8409359c14b110432f2b3cefb8fabe89b8
Jean Pimentel <jpimentel@ciandt.com>
2017-08-17 18:56:19 -0300

    Add user-agent interceptor

--- app/src/main/java/com/ciandt/dragonfly/example/config/Network.kt
+++ app/src/main/java/com/ciandt/dragonfly/example/config/Network.kt
@@ -1,9 +1,11 @@
 package com.ciandt.dragonfly.example.config
 
 import com.ciandt.dragonfly.example.BuildConfig
+import okhttp3.internal.Version as HttpClientVersion
 
 class Network {
     companion object {
+        val USER_AGENT: String = "Dragonfly/Android/${BuildConfig.VERSION_NAME} ${HttpClientVersion.userAgent()}"
         val BASE_URL: String = BuildConfig.BASE_URL
     }
 }
