54803f60b5e2a2543cd45d2e4bd524a6c828c19f
Jean Pimentel <jpimentel@ciandt.com>
2017-08-17 18:53:30 -0300

    Add Network configs

---
+++ app/src/main/java/com/ciandt/dragonfly/example/config/Network.kt
@@ -0,0 +1,9 @@
+package com.ciandt.dragonfly.example.config
+
+import com.ciandt.dragonfly.example.BuildConfig
+
+class Network {
+    companion object {
+        val BASE_URL: String = BuildConfig.BASE_URL
+    }
+}
