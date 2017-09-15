b991affb83a29eb1ab0a0f54cc14adb229530567
Ivam Luz <iluz@ciandt.com>
2017-09-15 15:02:15 -0300

    [SMTC-1464] Make sure correct Google Play Services is available on device before allowing user to sign in to the app

--- app/src/main/java/com/ciandt/dragonfly/example/infrastructure/extensions/Activity+Extension.kt
+++ app/src/main/java/com/ciandt/dragonfly/example/infrastructure/extensions/Activity+Extension.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.support.annotation.StringRes
 import android.support.design.widget.Snackbar
 import android.view.ViewGroup
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+
 
 fun Activity.getRootView(): ViewGroup {
     return window.decorView.findViewById(android.R.id.content)
@@ -15,4 +18,20 @@ fun Activity.showSnackbar(text: CharSequence, duration: Int = Snackbar.LENGTH_LO
 
 fun Activity.showSnackbar(@StringRes resId: Int, duration: Int = Snackbar.LENGTH_LONG) {
     showSnackbar(getString(resId), duration)
+}
+
+fun Activity.proceedOnlyIfRequiredGooglePlayServicesIsAvailable() {
+    val googleApiAvailability = GoogleApiAvailability.getInstance()
+    val googlePlayServicesCheck = googleApiAvailability.isGooglePlayServicesAvailable(this)
+
+    when (googlePlayServicesCheck) {
+        ConnectionResult.SERVICE_DISABLED,
+        ConnectionResult.SERVICE_INVALID,
+        ConnectionResult.SERVICE_MISSING,
+        ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED -> {
+            val dialog = googleApiAvailability.getErrorDialog(this, googlePlayServicesCheck, 0)
+            dialog.setOnCancelListener { this.finish() }
+            dialog.show()
+        }
+    }
 }
